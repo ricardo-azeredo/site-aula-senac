6e6401d770f421c2a48d41928773023f445f8e15
Ricardo <ricoazeredo@yahoo.com.br>
2023-05-25 11:33:58 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+ include "header.php";
+?>
+
+<body>
+    <header>
+        <h1>Aula Lean Startup</h1>
+    </header>
+    <main>
+        <div class="slide-links">            
+            <a
+             target="_blank" 
+             href="https://docs.google.com/presentation/d/11Uvi5Flx1YA4I8fPq8Ea6E5_brhTJVkT/edit?usp=sharing&ouid=114130750348912937035&rtpof=true&sd=true"
+            >
+                Aula 1 - Lean Startup
+            </a>
+            
+        </div>
+    </main>  
+    
+    
+</body>
+</html>
